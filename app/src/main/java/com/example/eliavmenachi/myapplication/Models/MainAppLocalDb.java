75085351ae0e4eabe24aac44a7b6c68174933799
Or Forte <or.forte1@gmail.com>
2018-07-17 21:16:41 +0300

    new dao

---
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Models/MainAppLocalDb.java
@@ -0,0 +1,19 @@
+package com.example.eliavmenachi.myapplication.Models;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+
+import com.example.eliavmenachi.myapplication.Entities.Sale;
+import com.example.eliavmenachi.myapplication.MyApplication;
+
+@Database(entities = {Sale.class}, version = 1)
+abstract class MainAppLocalDbRepository extends RoomDatabase {
+    public abstract SaleDao saleDao();
+}
+
+public class MainAppLocalDb{
+    static public MainAppLocalDbRepository db = Room.databaseBuilder(MyApplication.context,
+            MainAppLocalDbRepository.class,
+            "dbFileName.db").fallbackToDestructiveMigration().build();
+}
---
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Models/SaleDao.java
@@ -0,0 +1,24 @@
+package com.example.eliavmenachi.myapplication.Models;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.example.eliavmenachi.myapplication.Entities.Sale;
+import com.example.eliavmenachi.myapplication.Model.Student;
+
+import java.util.List;
+
+@Dao
+public interface SaleDao {
+    @Query("select * from Sale")
+    List<Sale> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Sale... sales);
+
+    @Delete
+    void delete(Sale sale);
+}
