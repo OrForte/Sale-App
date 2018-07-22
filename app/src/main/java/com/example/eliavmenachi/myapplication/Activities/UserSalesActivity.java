33184618a32ba821056e542eb147318a4e7c2608
Or Forte <or.forte1@gmail.com>
2018-07-22 12:30:25 +0300

    Nee activity

--- app/src/main/java/com/example/eliavmenachi/myapplication/Activities/UserSalesActivity.java
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Activities/UserSalesActivity.java
@@ -1,4 +1,27 @@
 package com.example.eliavmenachi.myapplication.Activities;
 
-public class UserSalesActivity {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
+import com.example.eliavmenachi.myapplication.Fragments.EditUserProfileFragment;
+import com.example.eliavmenachi.myapplication.R;
+
+public class UserSalesActivity extends Fragment {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("UserSalesActivity", "onCreate");
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_user_profile);
+
+        if (savedInstanceState == null) {
+            EditUserProfileFragment fragment = new EditUserProfileFragment();
+            FragmentTransaction tran = getSupportFragmentManager().beginTransaction();
+            tran.add(R.id.main_container, fragment);
+            //tran.addToBackStack("");
+            tran.commit();
+        }
+    }
 }
