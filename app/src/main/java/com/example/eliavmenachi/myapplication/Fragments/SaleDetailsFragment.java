7e6ebd9412912089bb64157765b5fe6184c06cb0
lior mizrahi <liorstrongxd@gmail.com>
2018-07-27 16:07:13 +0300

    das

--- app/src/main/java/com/example/eliavmenachi/myapplication/Fragments/SaleDetailsFragment.java
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Fragments/SaleDetailsFragment.java
@@ -1,4 +1,19 @@
 package com.example.eliavmenachi.myapplication.Fragments;
 
-public class SaleDetailsFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eliavmenachi.myapplication.R;
+
+public class SaleDetailsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sale_details, container, false);
+
+        return view;
+    }
 }
