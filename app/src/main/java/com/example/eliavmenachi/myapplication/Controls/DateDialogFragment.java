3694e77c95216d93498cf8bd021e6c4e7d5ff238
Or Forte <or.forte1@gmail.com>
2018-07-30 23:55:41 +0300

    Done some organizations

---
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Controls/DateDialogFragment.java
@@ -0,0 +1,39 @@
+package com.example.eliavmenachi.myapplication.Controls;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+
+public class DateDialogFragment extends DialogFragment {
+    DateDialogFragmentListener listener;
+    int year;
+    int month;
+    int day;
+
+    public void setDate(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+    }
+
+    interface DateDialogFragmentListener {
+        void onDateSet(int y, int m, int d);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                Log.d("TAG", "date selected: " + i);
+                if (listener != null) {
+                    listener.onDateSet(i, i1, i2);
+                }
+            }
+        }, year, month, day);
+        return dialog;
+    }
+}
---
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Models/Sale/SaleDao.java
@@ -0,0 +1,32 @@
+package com.example.eliavmenachi.myapplication.Models.Sale;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.example.eliavmenachi.myapplication.Entities.Sale;
+
+import java.util.List;
+
+@Dao
+public interface SaleDao {
+    @Query("select * from Sale where active = 1")
+    List<Sale> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Sale... sales);
+
+    @Delete
+    void delete(Sale sale);
+
+    @Query("SELECT * FROM Sale where id LIKE :id and  active = 1")
+    Sale getSaleBySaleId(String id);
+
+    @Query("SELECT * FROM Sale where storeId = :id and  active = 1")
+    List<Sale> getSaleByStoreId(int id);
+
+    @Query("SELECT * FROM Sale where userId LIKE :userName and  active = 1")
+    List<Sale> getSaleByUserName(String userName);
+}
