4f78094828fba36460a1ab461996777896580e36
lior mizrahi <liorstrongxd@gmail.com>
2018-08-01 22:21:27 +0300

    fix bug of forte haltura

--- app/src/main/java/com/example/eliavmenachi/myapplication/Activities/SalesActivity.java
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Activities/SalesActivity.java
@@ -74,6 +74,7 @@ public class SalesActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             progressBar.setVisibility(View.VISIBLE);
         }
