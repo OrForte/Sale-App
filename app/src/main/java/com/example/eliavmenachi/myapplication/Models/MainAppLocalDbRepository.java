bfb836a65361f27708eee84fb1f9794d799a89ca
lior mizrahi <liorstrongxd@gmail.com>
2018-08-03 21:33:22 +0300

    das

--- app/src/main/java/com/example/eliavmenachi/myapplication/Models/MainAppLocalDbRepository.java
+++ app/src/main/java/com/example/eliavmenachi/myapplication/Models/MainAppLocalDbRepository.java
@@ -14,7 +14,7 @@ import com.example.eliavmenachi.myapplication.Models.Sale.SaleDao;
 import com.example.eliavmenachi.myapplication.Models.User.UserDao;
 import com.example.eliavmenachi.myapplication.Models.User.UserPreviewDao;
 
-@Database(entities = {Sale.class, User.class, City.class, Mall.class, Store.class, UserPreview.class}, version = 11)
+@Database(entities = {Sale.class, User.class, City.class, Mall.class, Store.class, UserPreview.class}, version = 21)
 public abstract class MainAppLocalDbRepository extends RoomDatabase {
     public abstract SaleDao saleDao();
 
